296c2092cb74b4ef0c869530f54617f8d4263704
jonycayon@gmail.com <j224272j>
2015-07-10 22:47:30 +0200

    CSS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require("../controllers/quiz_controller")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
+
+/* Controller. */
+router.get('/quizes/question', quizController.question);
+
+/* Controller. */
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
